ad7debb1c84caf28734c62af5b701ccaf5bcdcb2
austinrafter <austin.rafter14@gmail.com>
2023-04-27 16:07:42 -0700

    feat: mdx rendering

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import "./App.css";
 import MdxTreeRenderer from "./components/mdx-tree-renderer";
 
 function App() {
-  return <MdxTreeRenderer filename={"~/test/mock-resume"} />;
+
+  return (
+      <MdxTreeRenderer filename={"~/test/mock-resume"} />
+  );
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import * as path from "path";
 
+const mdx = await import("@mdx-js/rollup")
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  optimizeDeps: {
+    include: ["react/jsx-runtime"],
+  },
+  plugins: [react(), mdx.default({ remarkPlugins: [] })],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, './src'),
     },
-  }
+  },
+
 });
