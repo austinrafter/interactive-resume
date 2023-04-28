361a21904e4ff09d2a734ff3e7f97b1efa80e402
austinrafter <austin.rafter14@gmail.com>
2023-04-28 13:23:49 -0700

    feat: added popup linkifier component that renders mdx

--- src/components/mdx-tree-renderer.tsx
+++ src/components/mdx-tree-renderer.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Resume from "../../test/mock/mock-resume.mdx";
+import PopupLinkifier from "./popup-linkifier";
+import Postman from "../../test/mock/postman.mdx";
 
 export default function MdxTreeRenderer(props: { filename: string }) {
+    const keywordMap = {postman: Postman}
   return (
-    <div>
-      <span>you passed {props.filename}</span>
-      <Resume />
-    </div>
+
+        <PopupLinkifier keywordMap={keywordMap}>
+            <Resume />
+        </PopupLinkifier>
+
   );
 }
---
+++ src/components/popup-linkifier.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface Props{
+    keywordMap : Record<string, React.ReactNode>
+    children: React.ReactNode
+}
+export default function PopupLinkifier(props: Props){
+
+    return(
+        <span>
+            {props.children}
+        </span>
+    );
+}
