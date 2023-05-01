4b627bd2bb7fc4b7dee67e2a09862d513d37f122
Joseph Burger <joseph@airkit.com>
2023-05-01 16:42:58 -0500

    chore: installed vitest

--- src/util/get-jsx-name.test.ts
+++ src/util/get-jsx-name.test.ts
@@ -1,1 +1,12 @@
-// @TODO
+import { describe, expect, test } from "vitest";
+import { getJsxNameFromRelativePath } from "./get-jsx-name";
+
+describe("getJsxNameFromRelativePath", () => {
+  test("should return the correct JSX name", () => {
+    const actual = getJsxNameFromRelativePath(
+      "../../test/mock/my-cool-file.mdx"
+    );
+
+    expect(actual).toBe("MyCoolFile");
+  });
+});
